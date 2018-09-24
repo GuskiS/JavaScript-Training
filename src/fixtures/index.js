aaa5112db7df99627e0174e5857ea66d6be7dd78
GuskiS <edvards@lazdans.lv>
2018-09-24 22:31:45 +0300

    LESSON-2 Finished

---
+++ src/fixtures/index.js
@@ -0,0 +1,13 @@
+import onIteration from "./onIteration";
+import onIsolation from "./onIsolation";
+import onLive from "./onLive";
+import onOverPopulation from "./onOverPopulation";
+import onReproduction from "./onReproduction";
+
+export default {
+  onIteration,
+  onIsolation,
+  onLive,
+  onOverPopulation,
+  onReproduction
+};
